80f64633631f479597f22d2c550f9fffec0fc609
João pedro Maciel pinheiro <shionlk98@gmail.com>
2023-08-24 14:14:46 -0300

    up 1

---
+++ main.js
@@ -0,0 +1,17 @@
+function tabuada() {
+  var num = document.querySelector("#txtn");
+  var tab = document.querySelector("#seltab");
+  if (num.value.lenght == 0) {
+    alert("por favor , digite um numero!");
+  } else {
+    let n = Number(num.value);
+    let c = 1;
+    tab.innerHTML = "";
+    while (c <= 10) {
+      let item = document.createElement("option");
+      item.text = `${n} x ${c} = ${n*c} `;
+      tab.appendChild(item);
+      c++;
+    }
+  }
+}
