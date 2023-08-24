c1adcf5fec65a35fc119fabfa07b41a6c9366f5a
João pedro Maciel pinheiro <shionlk98@gmail.com>
2023-08-24 14:42:44 -0300

    up 2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function tabuada() {
     tab.innerHTML = "";
     while (c <= 10) {
       let item = document.createElement("option");
-      item.text = `${n} x ${c} = ${n*c} `;
+      item.text = `${n} x ${c} = ${n * c} `;
       tab.appendChild(item);
       c++;
     }
